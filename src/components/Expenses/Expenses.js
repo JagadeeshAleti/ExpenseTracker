f56afbfd1cdbcb11e90cb943b2152628bd8a047b
Aleti <jagadeswararao.aleti@dxc.com>
2021-10-04 12:35:02 +0530

    Making Code More Linear

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -15,17 +16,7 @@ const Expenses = (props) => {
   const filteredExpense = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
-  let expenseContent = <p>No Content Here</p>;
-  if (filteredExpense.length > 0) {
-    expenseContent = filteredExpense.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
+  
   return (
     <div>
       <Card className="expenses">
@@ -33,7 +24,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expenseContent}
+        <ExpensesList items={filteredExpense} />
       </Card>
     </div>
   );
